0205101ded8754bdbe89dc3c734c633607fc5c1a
Bibek Aryal <bibekaryal86@gmail.com>
2024-02-01 19:32:44 -0700

    Calendar Components init

--- src/calendars/components/Calendar.tsx
+++ src/calendars/components/Calendar.tsx
@@ -1,7 +1,75 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
-const Calendar = (): React.ReactElement => {
+import { addNote, deleteNote, editNote, getStatusesList, GlobalState, StatusSchema, unmountPage } from '../../app'
+import { CourtCaseSchema, getCourtCases } from '../../cases'
+import { CALENDAR_OBJECT_TYPES } from '../../constants'
+import { FormSchema, getForms } from '../../forms'
+import { HearingTypeSchema, TaskTypeSchema } from '../../types'
+import { getHearingTypes } from '../../types/actions/hearingTypes.action'
+import { getTaskTypes } from '../../types/actions/taskTypes.action'
+import { editCalendar, getCalendar } from '../actions/calendars.action'
+import { CALENDARS_UNMOUNT } from '../types/calendars.action.types'
+import { HearingCalendarSchema, TaskCalendarSchema } from '../types/calendars.data.types'
+
+const mapStateToProps = ({ calendars, statuses, hearingTypes, taskTypes, courtCases, forms }: GlobalState) => {
+  return {
+    isForceFetch: calendars.isForceFetch,
+    selectedHearingCalendar: calendars.selectedHearingCalendar,
+    selectedTaskCalendar: calendars.selectedTaskCalendar,
+    statusList: statuses.statuses,
+    hearingTypesList: hearingTypes.hearingTypes,
+    taskTypesList: taskTypes.taskTypes,
+    courtCasesList: courtCases.courtCases,
+    formsList: forms.forms,
+  }
+}
+
+const mapDispatchToProps = {
+  getHearingCalendar: (calendarId: number) => getCalendar(calendarId, CALENDAR_OBJECT_TYPES.HEARING),
+  getTaskCalendar: (calendarId: number) => getCalendar(calendarId, CALENDAR_OBJECT_TYPES.TASK),
+  editHearingCalendar: (calendarId: number, calendar: HearingCalendarSchema) =>
+    editCalendar(calendarId, CALENDAR_OBJECT_TYPES.HEARING, calendar),
+  editTaskCalendar: (calendarId: number, calendar: TaskCalendarSchema) =>
+    editCalendar(calendarId, CALENDAR_OBJECT_TYPES.TASK, calendar),
+  unmountPage: () => unmountPage(CALENDARS_UNMOUNT),
+  getStatusesList: () => getStatusesList(),
+  addNote: (noteObjectType: string, noteObjectId: number, note: string) => addNote(noteObjectType, noteObjectId, note),
+  editNote: (noteObjectType: string, noteObjectId: number, note: string, noteId: number) =>
+    editNote(noteObjectType, noteObjectId, note, noteId),
+  deleteNote: (noteObjectType: string, noteId: number) => deleteNote(noteObjectType, noteId),
+  getHearingTypesList: () => getHearingTypes(),
+  getTaskTypesList: () => getTaskTypes(),
+  getCourtCasesList: () => getCourtCases(),
+  getFormsList: () => getForms(),
+}
+
+interface CalendarProps {
+  isForceFetch: boolean
+  selectedHearingCalendar: HearingCalendarSchema
+  getHearingCalendar: (calendarId: number) => void
+  selectedTaskCalendar: TaskCalendarSchema
+  getTaskCalendar: (calendarId: number) => void
+  editHearingCalendar: (calendarId: number, calendar: HearingCalendarSchema) => void
+  editTaskCalendar: (calendarId: number, calendar: TaskCalendarSchema) => void
+  unmountPage: () => void
+  statusList: StatusSchema<string>
+  addNote: (noteObjectType: string, noteObjectId: number, note: string) => void
+  editNote: (noteObjectType: string, noteObjectId: number, note: string, noteId: number) => void
+  deleteNote: (noteObjectType: string, noteId: number) => void
+  hearingTypesList: HearingTypeSchema[]
+  getHearingTypesList: () => void
+  taskTypesList: TaskTypeSchema[]
+  getTaskTypesList: () => void
+  courtCasesList: CourtCaseSchema[]
+  getCourtCasesList: () => void
+  formsList: FormSchema[]
+  getFormsList: () => void
+}
+
+const Calendar = (props: CalendarProps): React.ReactElement => {
+  console.log(props)
   const { id, type } = useParams()
   console.log(id, type)
   const pageText = () => (
@@ -13,4 +81,4 @@ const Calendar = (): React.ReactElement => {
   return <>{pageText()}</>
 }
 
-export default Calendar
+export default connect(mapStateToProps, mapDispatchToProps)(Calendar)
--- src/calendars/components/Calendars.tsx
+++ src/calendars/components/Calendars.tsx
@@ -1,6 +1,77 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const Calendars = (): React.ReactElement => {
+import { getStatusesList, GlobalState, StatusSchema, unmountPage } from '../../app'
+import { CourtCaseSchema, getCourtCases } from '../../cases'
+import { CALENDAR_OBJECT_TYPES } from '../../constants'
+import { FormSchema, getForms } from '../../forms'
+import { HearingTypeSchema, TaskTypeSchema } from '../../types'
+import { getHearingTypes } from '../../types/actions/hearingTypes.action'
+import { getTaskTypes } from '../../types/actions/taskTypes.action'
+import { addCalendar, deleteCalendar, editCalendar, getCalendars } from '../actions/calendars.action'
+import { CALENDARS_UNMOUNT } from '../types/calendars.action.types'
+import { HearingCalendarSchema, TaskCalendarSchema } from '../types/calendars.data.types'
+
+const mapStateToProps = ({ calendars, statuses, hearingTypes, taskTypes, courtCases, forms }: GlobalState) => {
+  return {
+    isForceFetch: calendars.isForceFetch,
+    isCloseModal: calendars.isCloseModal,
+    hearingCalendarsList: calendars.hearingCalendars,
+    taskCalendarsList: calendars.taskCalendars,
+    statusList: statuses.statuses,
+    hearingTypesList: hearingTypes.hearingTypes,
+    taskTypesList: taskTypes.taskTypes,
+    courtCasesList: courtCases.courtCases,
+    formsList: forms.forms,
+  }
+}
+
+const mapDispatchToProps = {
+  getHearingCalendars: () => getCalendars(CALENDAR_OBJECT_TYPES.HEARING),
+  getTaskCalendars: () => getCalendars(CALENDAR_OBJECT_TYPES.TASK),
+  addHearingCalendar: (calendar: HearingCalendarSchema) => addCalendar(calendar, CALENDAR_OBJECT_TYPES.HEARING),
+  addTaskCalendar: (calendar: TaskCalendarSchema) => addCalendar(calendar, CALENDAR_OBJECT_TYPES.TASK),
+  editHearingCalendar: (id: number, calendar: HearingCalendarSchema) =>
+    editCalendar(id, CALENDAR_OBJECT_TYPES.HEARING, calendar),
+  editTaskCalendar: (id: number, calendar: TaskCalendarSchema) =>
+    editCalendar(id, CALENDAR_OBJECT_TYPES.TASK, calendar),
+  deleteHearingCalendar: (id: number) => deleteCalendar(id, CALENDAR_OBJECT_TYPES.HEARING),
+  deleteTaskCalendar: (id: number) => deleteCalendar(id, CALENDAR_OBJECT_TYPES.TASK),
+  unmountPage: () => unmountPage(CALENDARS_UNMOUNT),
+  getStatusesList: () => getStatusesList(),
+  getHearingTypesList: () => getHearingTypes(),
+  getTaskTypesList: () => getTaskTypes(),
+  getCourtCasesList: () => getCourtCases(),
+  getFormsList: () => getForms(),
+}
+
+interface CalendarsProps {
+  isForceFetch: boolean
+  isCloseModal: boolean
+  hearingCalendarsList: HearingCalendarSchema[]
+  getHearingCalendars: () => void
+  taskCalendarsList: TaskCalendarSchema[]
+  getTaskCalendars: () => void
+  addHearingCalendar: (calendar: HearingCalendarSchema) => void
+  addTaskCalendar: (calendar: TaskCalendarSchema) => void
+  editHearingCalendar: (id: number, calendar: HearingCalendarSchema) => void
+  editTaskCalendar: (id: number, calendar: TaskCalendarSchema) => void
+  deleteHearingCalendar: (id: number) => void
+  deleteTaskCalendar: (id: number) => void
+  unmountPage: () => void
+  statusList: StatusSchema<string>
+  getStatusesList: () => void
+  hearingTypesList: HearingTypeSchema[]
+  getHearingTypesList: () => void
+  taskTypesList: TaskTypeSchema[]
+  courtCasesList: CourtCaseSchema[]
+  getCourtCasesList: () => void
+  formsList: FormSchema[]
+  getFormsList: () => void
+}
+
+const Calendars = (props: CalendarsProps): React.ReactElement => {
+  console.log(props)
   const pageText = () => (
     <>
       <h5>This is the Calendars!</h5>
@@ -10,4 +81,4 @@ const Calendars = (): React.ReactElement => {
   return <>{pageText()}</>
 }
 
-export default Calendars
+export default connect(mapStateToProps, mapDispatchToProps)(Calendars)
