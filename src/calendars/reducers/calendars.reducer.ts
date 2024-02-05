55eb93187a63fcc2b71bcbfc632c52b3a052bcad
Bibek Aryal <bibekaryal86@gmail.com>
2024-02-04 18:01:27 -0700

    calendars

--- src/calendars/reducers/calendars.reducer.ts
+++ src/calendars/reducers/calendars.reducer.ts
@@ -1,15 +1,9 @@
 import {
-  HEARING_CALENDAR_CREATE_SUCCESS,
-  HEARING_CALENDAR_DELETE_SUCCESS,
-  HEARING_CALENDAR_UPDATE_SUCCESS,
   HEARING_CALENDARS_RETRIEVE_REQUEST,
   HEARING_CALENDARS_RETRIEVE_SUCCESS,
   HEARING_CALENDARS_UNMOUNT,
   SET_SELECTED_HEARING_CALENDAR,
   SET_SELECTED_TASK_CALENDAR,
-  TASK_CALENDAR_CREATE_SUCCESS,
-  TASK_CALENDAR_DELETE_SUCCESS,
-  TASK_CALENDAR_UPDATE_SUCCESS,
   TASK_CALENDARS_RETRIEVE_REQUEST,
   TASK_CALENDARS_RETRIEVE_SUCCESS,
   TASK_CALENDARS_UNMOUNT,
@@ -36,6 +30,21 @@ export default function calendars(state = DefaultCalendarsState, action: Calenda
     }
   }
 
+  const matchesSuccessHc = /^HEARING_CALENDAR_(CREATE|UPDATE|DELETE)_SUCCESS$/.exec(action.type)
+  const matchesSuccessTc = /^TASK_CALENDAR_(CREATE|UPDATE|DELETE)_SUCCESS$/.exec(action.type)
+
+  if (matchesSuccessHc || matchesSuccessTc) {
+    return {
+        ...state,
+        isForceFetch: true,
+        isCloseModal: true,
+        hearingCalendars: [],
+        taskCalendars: [],
+        selectedHearingCalendar: DefaultHearingCalendarSchema,
+        selectedTaskCalendar: DefaultTaskCalendarSchema,
+      }
+  }
+
   switch (action.type) {
     case HEARING_CALENDARS_RETRIEVE_SUCCESS:
       return {
@@ -50,26 +59,6 @@ export default function calendars(state = DefaultCalendarsState, action: Calenda
         isForceFetch: false,
         isCloseModal: true,
         taskCalendars: action.taskCalendars,
-      }
-    case HEARING_CALENDAR_CREATE_SUCCESS:
-    case HEARING_CALENDAR_UPDATE_SUCCESS:
-    case HEARING_CALENDAR_DELETE_SUCCESS:
-      return {
-        ...state,
-        isForceFetch: true,
-        isCloseModal: true,
-        hearingCalendars: [],
-        selectedHearingCalendar: DefaultHearingCalendarSchema,
-      }
-    case TASK_CALENDAR_CREATE_SUCCESS:
-    case TASK_CALENDAR_UPDATE_SUCCESS:
-    case TASK_CALENDAR_DELETE_SUCCESS:
-      return {
-        ...state,
-        isForceFetch: true,
-        isCloseModal: true,
-        taskCalendars: [],
-        selectedTaskCalendar: DefaultTaskCalendarSchema,
       }
     case SET_SELECTED_HEARING_CALENDAR:
       return {
