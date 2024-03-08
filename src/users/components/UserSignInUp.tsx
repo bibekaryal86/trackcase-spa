acdefa911c0c22be6f9b1296c8f081a6da3e2437
Bibek Aryal <bibekaryal86@gmail.com>
2024-03-07 20:10:26 -0700

    Refactor init (User login)

--- src/users/components/UserSignInUp.tsx
+++ src/users/components/UserSignInUp.tsx
@@ -2,14 +2,13 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import Avatar from '@mui/material/Avatar'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
-import Grid from '@mui/material/Grid'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import { Navigate, useLocation, useNavigate } from 'react-router-dom'
 
-import { getErrMsg, isLoggedIn, Link, LocalStorage, resetAlert, resetSpinner, setAlert, setSpinner } from '../../app'
+import { getErrMsg, isLoggedIn, LocalStorage, resetAlert, resetSpinner, setAlert, setSpinner } from '../../app'
 import { ALERT_TYPE_FAILURE, ALERT_TYPE_INFO, ALERT_TYPE_WARNING, INVALID_SIGNIN, SIGNIN_FIRST } from '../../constants'
 import { login } from '../action/users.action'
 import { AppUserLoginResponse } from '../types/users.data.types'
@@ -32,6 +31,9 @@ const mapDispatchToProps = {
 const UserSignInUp = (props: LoginProps): React.ReactElement => {
   const { setAlert, resetAlert, setSpinner, resetSpinner } = props
 
+  const [isShowSignin, setIsShowSignin] = useState(true)
+  const [isShowReset, setIsShowReset] = useState(false)
+
   const userLoginSuccessLocalStorageActions = (appUserLoginResponse: AppUserLoginResponse) => {
     LocalStorage.setItem('token', appUserLoginResponse.token)
     LocalStorage.setItem('tokenExpiration', new Date().setMinutes(new Date().getMinutes() + 15))
@@ -42,14 +44,15 @@ const UserSignInUp = (props: LoginProps): React.ReactElement => {
   const { state } = useLocation() as { state: { redirect: string; message: string } }
   const navigate = useNavigate()
 
-  const redirect = useCallback(() => {
-    const pageToRedirectTo = state?.redirect
-    if (pageToRedirectTo) {
-      return <Navigate to={pageToRedirectTo} />
-    } else {
-      return <Navigate to="/home" />
+  useEffect(() => {
+    if (state?.message?.length) {
+      setAlert(ALERT_TYPE_INFO, state.message)
     }
-  }, [state])
+    if (state?.redirect && !isLoggedIn()) {
+      setAlert(ALERT_TYPE_WARNING, SIGNIN_FIRST)
+    }
+    // state.message = ''
+  }, [setAlert, state])
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     setSpinner()
@@ -75,11 +78,10 @@ const UserSignInUp = (props: LoginProps): React.ReactElement => {
     } else {
       setAlert(ALERT_TYPE_FAILURE, INVALID_SIGNIN)
     }
-
     resetSpinner()
   }
 
-  const loginForm = () => {
+  const signInUpForm = () => {
     return (
       <Box
         sx={{
@@ -93,11 +95,25 @@ const UserSignInUp = (props: LoginProps): React.ReactElement => {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign in
+          {isShowSignin ? `Sign in` : isShowReset ? `Reset Password` : `Sign up`}
         </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField margin="normal" required fullWidth id="username" label="User Name" name="username" autoFocus />
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          noValidate
+          sx={{ marginTop: 2, display: 'flex', flexDirection: 'column', alignItems: 'center', width: '25%' }}
+        >
           <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Email"
+            name="username"
+            placeholder="Your Email is your username"
+            autoFocus
+          />
+          {!isShowReset && <><TextField
             margin="normal"
             required
             fullWidth
@@ -106,33 +122,39 @@ const UserSignInUp = (props: LoginProps): React.ReactElement => {
             type="password"
             id="password"
           />
+          {!isShowSignin && (
+            <TextField margin="normal" required fullWidth id="fullName" label="Full Name" name="fullName" />
+          )}</>}
           <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-            Sign In
+            {isShowSignin ? `Submit Sign in` : isShowReset ? `Submit Password Reset` : `Submit Sign up`}
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link text="Forgot Password?" navigateToPage="/forgot-password" />
-            </Grid>
-            <Grid item xs>
-              <Link text="Don't Have an Account? Sign Up!" navigateToPage="/sign-up" />
-            </Grid>
-          </Grid>
+          <Button fullWidth variant="text" sx={{ mt: 3 }} onClick={() => {
+            setIsShowSignin(!isShowSignin)
+            setIsShowReset(false)
+          }}>
+            {isShowSignin ? `Don't have account? Click here to Sign up` : `Have account? Click here to sign in`}
+          </Button>
+          {!isShowReset && <Button fullWidth variant="text" onClick={() => {
+            setIsShowReset(!isShowReset)
+            setIsShowSignin(false)
+          }}>
+            Forgot Password? Click here to reset
+          </Button>}
         </Box>
       </Box>
     )
   }
 
-  useEffect(() => {
-    if (state?.message?.length) {
-      setAlert(ALERT_TYPE_INFO, state.message)
+  const redirect = useCallback(() => {
+    const pageToRedirectTo = state?.redirect
+    if (pageToRedirectTo) {
+      return <Navigate to={pageToRedirectTo} />
+    } else {
+      return <Navigate to="/home" />
     }
-    if (state?.redirect && !isLoggedIn()) {
-      setAlert(ALERT_TYPE_WARNING, SIGNIN_FIRST)
-    }
-    // state.message = ''
-  }, [setAlert, state])
+  }, [state])
 
-  return <>{isLoggedIn() ? redirect() : loginForm()}</>
+  return <>{isLoggedIn() ? redirect() : signInUpForm()}</>
 }
 
 export default connect(null, mapDispatchToProps)(UserSignInUp)
