c27410f3863c53d9bd61444306e2faa284f1ce92
Bibek Aryal <bibekaryal86@gmail.com>
2024-03-26 20:25:22 -0600

    Judges

--- src/judges/reducers/judges.reducer.ts
+++ src/judges/reducers/judges.reducer.ts
@@ -1,55 +1,25 @@
-import { CLIENT_CREATE_SUCCESS, CLIENT_DELETE_SUCCESS, CLIENT_UPDATE_SUCCESS } from '../../clients'
 import {
-  JUDGE_CREATE_SUCCESS,
-  JUDGE_DELETE_SUCCESS,
-  JUDGE_UPDATE_SUCCESS,
-  JUDGES_READ_REQUEST,
+  JUDGES_CREATE_SUCCESS,
+  JUDGES_DELETE_SUCCESS,
   JUDGES_READ_SUCCESS,
-  JUDGES_UNMOUNT,
-  SET_SELECTED_JUDGE,
+  JUDGES_UPDATE_SUCCESS,
 } from '../types/judges.action.types'
-import { DefaultJudgeSchema, DefaultJudgeState, JudgesAction, JudgesState } from '../types/judges.data.types'
+import { DefaultJudgeState, JudgesAction, JudgesState } from '../types/judges.data.types'
 
 export default function judges(state = DefaultJudgeState, action: JudgesAction): JudgesState {
-  const matchesRequest = /^JUDGE_(CREATE|UPDATE|DELETE)_REQUEST$/.exec(action.type)
-  if (matchesRequest || action.type === JUDGES_READ_REQUEST) {
-    return {
-      ...state,
-      isCloseModal: false,
-    }
-  }
-
   switch (action.type) {
     case JUDGES_READ_SUCCESS:
       return {
         ...state,
-        isCloseModal: true,
         judges: action.judges,
+        requestMetadata: action.requestMetadata,
       }
-    case JUDGE_CREATE_SUCCESS:
-    case JUDGE_UPDATE_SUCCESS:
-    case JUDGE_DELETE_SUCCESS:
+    case JUDGES_CREATE_SUCCESS:
+    case JUDGES_UPDATE_SUCCESS:
+    case JUDGES_DELETE_SUCCESS:
       return {
-        isCloseModal: true,
         judges: [],
-        selectedJudge: DefaultJudgeSchema,
-      }
-    case SET_SELECTED_JUDGE:
-      return {
-        ...state,
-        selectedJudge: action.selectedJudge,
-      }
-    case CLIENT_CREATE_SUCCESS:
-    case CLIENT_UPDATE_SUCCESS:
-    case CLIENT_DELETE_SUCCESS:
-      return {
-        ...state,
-        selectedJudge: DefaultJudgeSchema,
-      }
-    case JUDGES_UNMOUNT:
-      return {
-        ...state,
-        selectedJudge: DefaultJudgeSchema,
+        requestMetadata: {},
       }
     default:
       return state
--- src/judges/types/judges.data.types.ts
+++ src/judges/types/judges.data.types.ts
@@ -1,58 +1,86 @@
-import { BaseModelSchema, ResponseBase, StatusBaseSchema } from '../../app'
+import { BaseModelSchema, FetchRequestMetadata, ResponseBase } from '../../app'
 import { ClientSchema } from '../../clients'
 import { ID_DEFAULT } from '../../constants'
 import { CourtSchema } from '../../courts'
+import { ComponentStatusSchema } from '../../types'
 
-export interface JudgeSchema extends StatusBaseSchema, BaseModelSchema {
+export interface JudgeBase {
   name: string
   webex?: string
-  courtId: number
+  courtId:  number
+  componentStatusId: number
+  comments?: string
+}
+
+export interface JudgeSchema extends JudgeBase, BaseModelSchema {
   // orm_mode
   court?: CourtSchema
+  componentStatus?: ComponentStatusSchema
   clients?: ClientSchema[]
   // history
   historyJudges?: HistoryJudgeSchema[]
 }
 
 export interface JudgeResponse extends ResponseBase {
-  judges: JudgeSchema[]
+  data: JudgeSchema[]
 }
 
-export interface HistoryJudgeSchema extends BaseModelSchema {
-  userName: string
+export interface HistoryJudgeSchema extends Partial<JudgeBase>, BaseModelSchema {
+  appUserId: number
   judgeId: number
-  // from judge schema, need everything optional here so can't extend
-  name?: string
-  webex?: string
-  courtId?: number
-  status?: string
-  comments?: string
   // orm_mode
   judge?: JudgeSchema
   court?: CourtSchema
+  componentStatus?: ComponentStatusSchema
 }
 
 export interface JudgesState {
-  isCloseModal: boolean
   judges: JudgeSchema[]
-  selectedJudge: JudgeSchema
+  requestMetadata: Partial<FetchRequestMetadata>
 }
 
 export interface JudgesAction extends JudgesState {
   type: string
 }
 
+export interface JudgeFormData extends JudgeSchema {
+  isHardDelete: boolean
+  isShowSoftDeleted: boolean
+}
+
+export interface JudgeFormErrorData extends JudgeSchema {
+  courtError: string
+  componentStatusError: string
+}
+
 export const DefaultJudgeSchema: JudgeSchema = {
   name: '',
   webex: '',
-
   courtId: ID_DEFAULT,
-  status: '',
+  componentStatusId: ID_DEFAULT,
   comments: '',
 }
 
-export const DefaultJudgeState: JudgesState = {
-  isCloseModal: true,
+export const DefaultFormState: JudgesState = {
   judges: [],
-  selectedJudge: DefaultJudgeSchema,
+  requestMetadata: {},
+}
+
+export const DefaultJudgeFormData: JudgeFormData = {
+  ...DefaultJudgeSchema,
+  id: ID_DEFAULT,
+  isHardDelete: false,
+  isShowSoftDeleted: false,
+  isDeleted: false,
+}
+
+export const DefaultJudgeFormErrorData: JudgeFormErrorData = {
+  ...DefaultJudgeFormData,
+  courtError: '',
+  componentStatusError: '',
+}
+
+export const DefaultJudgeState: JudgesState = {
+  judges: [],
+  requestMetadata: {},
 }
