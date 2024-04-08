2952732e3480e099d73f3cfcd0b0d59337aee73d
Bibek Aryal <bibekaryal86@gmail.com>
2024-04-08 16:54:20 -0600

    relative-imports

--- src/home/components/Home.tsx
+++ src/home/components/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import { connect } from 'react-redux'
 
-import { getRefTypes } from '../../types'
+import { getRefTypes } from '@ref_types/actions/refTypes.action'
 
 const mapDispatchToProps = {
   getRefTypes: () => getRefTypes(),
